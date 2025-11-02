35fe6060b8faf4513a44274ada961b7a970e8c77
lettuce <57018651+lilianzlettuce@users.noreply.github.com>
2025-11-02 04:06:40 -0500

    try fixing weird page path bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://spill.purduehackers.com/',
+  site: 'https://spill.purduehackers.com',
   output: 'server',
   adapter: vercel(),
   integrations: [react()],
